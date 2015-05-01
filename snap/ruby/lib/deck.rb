7bcdd63a85a77308a361b8cc3b66ef69c87716f5
Stephen McCullough <stephen@sixdegreelabs.com>
2015-05-01 23:18:35 +0100

    #2 - Actually add the deck.rb file

---
+++ snap/ruby/lib/deck.rb
@@ -0,0 +1,23 @@
+require 'card'
+
+class Deck 
+
+  def initialize() 
+    @deck = []
+    Card::SUITS.each do |suit|
+      Card::RANKS.each do |rank|
+        @deck << Card.new(rank, suit)
+      end
+    end
+  end
+  attr_reader :deck
+
+  def count
+    @deck.length
+  end
+
+  def shuffle 
+    @deck.shuffle
+  end
+
+end
